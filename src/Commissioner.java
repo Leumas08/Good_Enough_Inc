7d650e8d7c4ff6d070c3b730531a51631766795c
Samuel Price <swp99piano@gmail.com>
2019-04-01 18:44:14 -0400

    Tester

---
+++ src/Commissioner.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author swp99
+ */
+public class Commissioner {
+    private String name;
+    private String email;
+    
+    public Commissioner(String name, String email)
+    {
+        this.name=name;
+        this.email=email;
+    }
+}
