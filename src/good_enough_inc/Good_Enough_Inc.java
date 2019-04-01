f2ff2b156c7a68a514989d7378fcea4b5a930e11
Samuel Price <swp99piano@gmail.com>
2019-04-01 17:26:17 -0400

    JFrame

---
+++ src/good_enough_inc/Good_Enough_Inc.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package good_enough_inc;
+
+/**
+ *
+ * @author swp99
+ */
+public class Good_Enough_Inc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
